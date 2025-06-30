bbf58770700c1f845182e89bea7218abf4c9fa8a
joyce-costa90 <joy_costa15@hotmail.com>
2025-06-30 19:39:16 -0300

    Projeto

--- database.py
+++ database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from settings import DATABASE_URL
 
 # Se não houver DATABASE_URL configurada, usa SQLite
-if not DATABASE_URL or DATABASE_URL == "mysql+pymysql://root:root@localhost:3306/test_schema":
+if not DATABASE_URL or DATABASE_URL == "mysql+pymysql://root:root@localhost:3306/meu_projeto":
     DATABASE_URL = "sqlite:///./biblioteca.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
